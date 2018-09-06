fc03a697c8c3f00f42f59fb4a5e1c578ed3eb49b
thej <thej.kodiyat@rackspace.com>
2018-09-06 14:09:39 -0500

    complete reduce() info

--- lambda.py
+++ lambda.py
@@ -1,8 +1,11 @@
 ### Lambda ###
 
-# Lambdas are one line functions. They are also known as anonymous functions in some other languages. You might want to use lambdas when you don't want to use a function twice on a program. They are just like normal functions and even behave like them.
+# Lambdas are one line functions. They are also known as anonymous functions in some other languages. 
+# You might want to use lambdas when you don't want to use a function twice on a program. They are just 
+# like normal functions and even behave like them.
 
-# Lambda function can take any number of arguments, but can only have one expression. They are mainly used in combination with the functions filter(), map() and reduce().
+# Lambda function can take any number of arguments, but can only have one expression. They are mainly 
+# used in combination with the functions filter(), map() and reduce().
 
 x = lambda a : a + 10
 print(x(5))
@@ -19,7 +22,9 @@ print(mytripler(11))
 
 ### Map ###
 # r = map(func, seq)
-# The first argument `func` is the name of a function and the second a sequence (e.g. a list) seq. `map()` applies the function `func` to all the elements of the sequence `seq`. It returns a new list with the elements changed by `func`.
+# The first argument `func` is the name of a function and the second a sequence (e.g. a list) seq. `map()` 
+# applies the function `func` to all the elements of the sequence `seq`. It returns a new list with the elements 
+# changed by `func`.
 
 def fahrenheit(T):
     return ((float(9)/5)*T + 32)
@@ -51,8 +56,11 @@ print(i)
 # [17,10,19,23]
 
 ### Filter ###
-# The function filter(function, list) offers an elegant way to filter out all the elements of a list, for which the function `function` returns True.
-# The function filter(f,l) needs a function f as its first argument. f returns a Boolean value, i.e. either True or False. This function will be applied to every element of the list `l`. Only if f returns True will the element of the list be included in the result list.
+# The function filter(function, list) offers an elegant way to filter out all the elements of a list, for which 
+# the function `function` returns True.
+# The function filter(f,l) needs a function f as its first argument. f returns a Boolean value, i.e. either True 
+# or False. This function will be applied to every element of the list `l`. Only if f returns True will the element 
+# of the list be included in the result list.
 
 fib = [0,1,1,2,3,5,8,13,21,34,55]
 result = filter(lambda x : x%2, fib)
@@ -64,3 +72,18 @@ print(result)
 
 ### Reduce ###
 # The function reduce(func, seq) continually applies the function func() to the sequence seq. It returns a single value.
+# If seq = [s1, s2, s3, ..., sn], calling reduce(func, seq) works like this:
+#  * At first the first two elements of seq will be applied to func, i.e. func(s1,s2) The list on which reduce() works looks 
+#    now like this: [ func(s1, s2), s3, ..., sn]
+#  * In the next step func will be applied on the previous result and the third element of the list, i.e. func(func(s1,s2),s3)
+#    The list looks like this now: [ func(func(s1, s2),s3), ... , sn]
+#  * Continue like this until just one element is left and return this element as the result of reduce()
+
+R = reduce(lambda x,y: x+y, [47,11,42,13])
+print(R)
+
+f = reduce(lambda x,y: x if x>y else y, [48,11,42,102,13])
+print(f)
+
+s = reduce(lambda x,y: x+y, range(1,101))
+print(s)
